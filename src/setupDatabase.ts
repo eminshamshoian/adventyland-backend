18c873a846b3f0250db240616b5c5a1701725ee3
eminshamshoian <eminshamshoian@gmail.com>
2023-01-31 21:38:41 -0800

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+// Ananymous function so it can be imported with any name
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Connected to database');
+      })
+      .catch((error) => {
+        log.error('Database connection error', error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnected', connect);
+};
